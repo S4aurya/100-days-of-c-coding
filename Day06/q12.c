d6b1c497da68540fc670402d4bec9680a5bf6636
Shaurya <anshsingh26sharuya@gmail.com>
2026-08-15 18:47:00 +0530

    Day 06: Solved Q11 and Q12

---
+++ Day06/q12.c
@@ -0,0 +1,38 @@
+/*
+Q12 (Conditional Statements): Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        if (n > 0) {
+            printf("Positive\n");
+        } else {
+            if (n < 0) {
+                printf("Negative\n");
+            } else {
+                printf("Zero\n");
+            }
+        }
+    }
+    return 0;
+}
