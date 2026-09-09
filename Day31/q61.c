88f5246bf6b415358edab130856eafb04e71cbcd
Shaurya <anshsingh26sharuya@gmail.com>
2026-09-09 19:22:00 +0530

    Day 31: Solved Q61 and Q62

---
+++ Day31/q61.c
@@ -0,0 +1,45 @@
+/*
+Q61 (Arrays (1D)): Search for an element in an array using linear search.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+3
+Output 1:
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int arr[1000];
+        for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+        int target;
+        if (scanf("%d", &target) == 1) {
+            int found = -1;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == target) {
+                    found = i;
+                    break;
+                }
+            }
+            if (found != -1) {
+                printf("Found at index %d\n", found);
+            } else {
+                printf("-1\n");
+            }
+        }
+    }
+    return 0;
+}
---
+++ Day31/q62.c
@@ -0,0 +1,33 @@
+/*
+Q62 (Arrays (1D)): Reverse an array without taking extra space.
+
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int arr[1000];
+        for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        for (int i = 0; i < n; i++) {
+            printf("%d%c", arr[i], (i == n - 1 ? '\n' : ' '));
+        }
+    }
+    return 0;
+}
