e4576825bd0d9ffcf6a482b1a5f1e81a900d462f
Shaurya <anshsingh26sharuya@gmail.com>
2026-09-12 19:43:00 +0530

    Day 34: Solved Q67 and Q68

---
+++ Day34/q67.c
@@ -0,0 +1,34 @@
+/*
+Q67 (Arrays (1D)): Insert an element in an array at a given position.
+
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int arr[1000];
+        for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+        int pos, val;
+        if (scanf("%d %d", &pos, &val) == 2) {
+            for (int i = n; i > pos; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[pos] = val;
+            n++;
+            for (int i = 0; i < n; i++) {
+                printf("%d%c", arr[i], (i == n - 1 ? '\n' : ' '));
+            }
+        }
+    }
+    return 0;
+}
